c7e14b679e6ad4e2b13a4168ef1ff04c4b5d0c25
Jesse Stuart <jdstuart@icloud.com>
2017-08-01 08:52:51 -0400

    Init commit.

---
+++ functions/tt-apex-gradle/src/main/groovy/com/todaytix/lambda/Example.groovy
@@ -0,0 +1,17 @@
+package com.todaytix.lambda
+
+import com.amazonaws.services.lambda.runtime.Context
+
+class Example {
+  static class ExampleRequest {
+    def hello
+  }
+
+  static class ExampleResponse {
+    def hello
+  }
+
+  ExampleResponse handler(ExampleRequest event, Context context) {
+    new ExampleResponse(hello: event.hello)
+  }
+}
